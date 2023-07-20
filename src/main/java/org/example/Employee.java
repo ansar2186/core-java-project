8ad4c50da24cebe52bb6868570a60d4a7e79b0bd
Ansar Ahmad <ansar.ahmad@dxc.com>
2023-07-20 12:10:52 +0530

    Modified the Employee class

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -4,32 +4,71 @@ public class Employee {
 
     int id;
     String name;
+    String title;
+    double salary;
+
+    String location;
+
 
     public Employee() {
     }
 
-    public Employee(int id, String name) {
+    public Employee(int id, String name, String title, double salary, String location) {
         this.id = id;
         this.name = name;
+        this.title = title;
+        this.salary = salary;
+        this.location = location;
     }
 
     public int getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public double getSalary() {
+        return salary;
+    }
 
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", title='" + title + '\'' +
+                ", salary=" + salary +
+                ", location='" + location + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,32 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-
-
+        // IntelliJ IDEA suggests fixing it
         Employee employee = new Employee();
         employee.setId(1);
         employee.setName("Ansar");
-        System.out.printf("Hello and welcome!" + employee.getName());
+        System.out.printf("Hello and welcome %s" , employee.getName());
 
+        List<Employee> employeeList= new ArrayList<>();
+        employeeList.add(new Employee(1,"Ansar","software Engineer",205000,"Noida"));
+        employeeList.add(new Employee(2,"Lubna","software Engineer",95000,"Noida"));
+        employeeList.add(new Employee(1,"Anuj","software Engineer",85000,"Noida"));
+        employeeList.add(new Employee(1,"Afjal","software Engineer",75000,"Delhi"));
+        employeeList.add(new Employee(1,"Faiz","Manager",65000,"Delhi"));
+        employeeList.add(new Employee(1,"Shariq","Banker",35000,"Mumbai"));
+        employeeList.add(new Employee(1,"Tayyab","businnes man",55000,"Dehradun"));
+
+        Map<String, List<Employee>> collect = employeeList.stream().collect(Collectors.groupingBy(Employee::getLocation));
+        System.out.println(collect);
 
     }
 }
